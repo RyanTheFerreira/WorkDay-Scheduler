0b2a3ba8620d200fea5c482216d4c4da915c5f33
RyanTheFerreira <108907295+RyanTheFerreira@users.noreply.github.com>
2023-04-20 22:50:58 -0400

    added timed color change

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,24 @@ var time = today.format('H');
 console.log(time);
 $('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm A'));
 
- 
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+$(document).ready(function() {
+  var now = new Date();
+  var hour = now.getHours();
   
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < hour) {
+      $(this).addClass("past");
+    } else if (blockHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+});
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -34,4 +41,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
